8f262ba74c2caf09b8aae64669949330aa6cb913
Mkfamily32 <93978832+Mkfamily32@users.noreply.github.com>
2022-01-26 21:03:02 +0300

    added homework2

---
+++ homework2/script.js
@@ -0,0 +1,28 @@
+// Переменная, которая хранит имя
+
+let userName = 'Ira';
+console.log(userName);
+
+// Переменная хранит возраст
+
+let userAge = '42';
+console.log(userAge);
+
+// Переменная хранит фамилию
+
+let userLastName = 'Makiy';
+console.log(userLastName);
+
+let newUser = 'Lok';
+console.log(newUser);
+
+let shoppingList;
+let isUserOnline;
+let employeeSalary;
+let cursorPos;
+
+let message='Hello world!';
+console.log(message);
+
+let value = 42;
+console.log(`value`, value);
